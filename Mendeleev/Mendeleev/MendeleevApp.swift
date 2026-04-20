f849fecb1fd25751c0ad8755cf61b1408fecdc5d
Piumra Prathiban <piumrawork@gmail.com>
2026-04-20 20:47:19 +0530

    Initialization part 2

---
+++ Mendeleev/Mendeleev/MendeleevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MendeleevApp.swift
+//  Mendeleev
+//
+//  Created by Piumra Prathiban on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct MendeleevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mendeleev/MendeleevTests/MendeleevTests.swift
@@ -0,0 +1,18 @@
+//
+//  MendeleevTests.swift
+//  MendeleevTests
+//
+//  Created by Piumra Prathiban on 2026-04-20.
+//
+
+import Testing
+
+struct MendeleevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
