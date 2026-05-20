761ea278668d24d4eb7f2fa4a48eaf39b38c5d9b
Piumra Prathiban <piumrawork@gmail.com>
2026-05-20 15:22:27 +0530

    Expose backups API in preload script

--- dist-electron/preload.cjs
+++ dist-electron/preload.cjs
@@ -8,4 +8,10 @@ const api = {
   remove: (id) => electron.ipcRenderer.invoke("notes:delete", id),
   search: (query) => electron.ipcRenderer.invoke("notes:search", query)
 };
+const backups = {
+  info: () => electron.ipcRenderer.invoke("backups:info"),
+  export: () => electron.ipcRenderer.invoke("backups:export"),
+  restore: () => electron.ipcRenderer.invoke("backups:restore")
+};
 electron.contextBridge.exposeInMainWorld("notes", api);
+electron.contextBridge.exposeInMainWorld("backups", backups);
